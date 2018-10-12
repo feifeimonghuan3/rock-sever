62792178a6902e361248ca222a0a37f77327c64c
林润佳 <john.lin@antcosa.com>
2018-10-12 18:44:16 +0800

    add html

---
+++ public/precache-manifest.021a5b9a34cfe42abb07d8c9582d52d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1188e2d3cf5ea50eef8a17c824e2afbe",
+    "url": "/static/media/arrow-right2.1188e2d3.svg"
+  },
+  {
+    "revision": "8ea9f747f4a83a830d04ed3e3b36754a",
+    "url": "/static/media/arrow-left2.8ea9f747.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "094d20d0b18e04725a00",
+    "url": "/static/js/main.094d20d0.chunk.js"
+  },
+  {
+    "revision": "44d8621cb6578a7f65ff",
+    "url": "/static/js/1.44d8621c.chunk.js"
+  },
+  {
+    "revision": "094d20d0b18e04725a00",
+    "url": "/static/css/main.1d02a0cc.chunk.css"
+  },
+  {
+    "revision": "8d44994a952597c3c5c756762f70931d",
+    "url": "/index.html"
+  }
+];
